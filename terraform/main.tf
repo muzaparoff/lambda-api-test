4171d891e4aa9bafaf994bbd86ff7a474f667bb0
muzaparoff <muzyukins@gmail.com>
2023-09-20 21:52:54 +0300

    [ADD]terraform and github actions deployment step

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,9 @@
-# Configure AWS provider
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
 }
 
-# Create a Lambda function
 resource "aws_lambda_function" "hello_lambda" {
   function_name = var.lambda_function_name
   runtime       = var.lambda_runtime
@@ -14,7 +12,6 @@ resource "aws_lambda_function" "hello_lambda" {
   role          = aws_iam_role.lambda_role.arn
 }
 
-# Create an IAM role for the Lambda function
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_execution_role"
 
@@ -35,26 +32,22 @@ resource "aws_iam_role" "lambda_role" {
 EOF
 }
 
-# Attach a basic execution policy to the Lambda role
 resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.lambda_role.name
 }
 
-# Create an API Gateway REST API
 resource "aws_api_gateway_rest_api" "my_api" {
   name        = "my-api"
   description = "My API"
 }
 
-# Create a resource for the API
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
   path_part   = "hello"
 }
 
-# Create a GET method for the resource
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.my_api.id
   resource_id   = aws_api_gateway_resource.resource.id
@@ -62,7 +55,6 @@ resource "aws_api_gateway_method" "method" {
   authorization = "NONE"
 }
 
-# Create a Lambda integration for the GET method
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   resource_id = aws_api_gateway_resource.resource.id
@@ -73,14 +65,12 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                    = aws_lambda_function.hello_lambda.invoke_arn
 }
 
-# Deploy the API
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   stage_name = "prod"
 }
 
-# Create an output to display the API Gateway URL
 output "api_gateway_url" {
   value = aws_api_gateway_deployment.deployment.invoke_url
 }
